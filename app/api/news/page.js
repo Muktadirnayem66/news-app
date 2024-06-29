b8cefe2ffe60e673fa8f6eb99c7cfaf765da3161
Muktadirnayem66 <mmuktadir629@gmail.com>
2024-06-29 23:00:18 +0600

    some changes in news pages

--- app/api/news/page.js
+++ app/api/news/page.js
@@ -3,17 +3,17 @@ import { Getnews } from "@/lib/get-news";
 import Link from "next/link";
 
 const AllNews = async () => {
-  let  allnews = await Getnews("business");
+  const   allnews = await Getnews("business");
   
   return (
     <div>
-      { allnews.articles.length > 0 ? (
+      { 
         allnews.articles.map((item, i) => (
             <Link href={`news/${item.author}`} key={i}>
               <NewsCard item={item} />
             </Link>
           ))
-      ) : ( <p>No Data Available</p>)}
+      }
     </div>
   );
 };
